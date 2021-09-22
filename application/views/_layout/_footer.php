318c24c1aee8ae805a54a28403a661dedf3c33a6
GhozyanH.Prdn <ghozyan20.ozy@gmail.com>
2021-09-23 06:37:03 +0700

    first commit

---
+++ application/views/_layout/_footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+	<!-- To the right -->
+	<div class="pull-right hidden-xs">
+		Dashboard Admin
+	</div>
+	<!-- Default to the left -->
+	<strong>Copyright © 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+</footer>
